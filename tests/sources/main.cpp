a74fc8504bb29ecc7294472fcd275f62f85df7c0
Rafael Tonello <tonello.rafinha@gmail.com>
2025-03-04 14:57:52 -0300

    mend
    add RamCacheDB tests

--- tests/sources/main.cpp
+++ tests/sources/main.cpp
@@ -15,6 +15,7 @@
 #include "./Controller/Internal/Controller_VarHelper.test.h"
 #include "./Controller/Controller.test.h"
 #include "./Shared/Misc/TaggedObject/TaggedObject.test.h"
+#include "./Services/Storage/RamCacheDB/ramcachedb.test.h"
 #include <http.test.h>
 
 using namespace std;
@@ -36,6 +37,7 @@ int main(int argc, char* argv[]){
         testers.push_back(new ControllerTester());
         testers.push_back(new TaggedObjectTester());
         testers.push_back(new Http_test());
+        testers.push_back(new RamCacheDBTest());
 
         //[ ]logger
         //[ ]  LoggerFileWriter
