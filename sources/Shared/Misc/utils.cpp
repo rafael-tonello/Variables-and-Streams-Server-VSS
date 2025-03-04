db71230b0d6db2c2c87d12798a110044383b3a63
Rafael Tonello <tonello.rafinha@gmail.com>
2025-03-04 14:54:27 -0300

    improves utils.cpp code

--- sources/Shared/Misc/utils.cpp
+++ sources/Shared/Misc/utils.cpp
@@ -199,7 +199,7 @@ future<string> Utils::httpGet(string url, map<string, string> headers)
     return std::async(std::launch::async, [](string cmd2){
         auto ret = ssystem(cmd2);
         if (ret.output.find("curl") == string::npos)
-            return ret;
+            return ret.output;
         else
             throw std::runtime_error("Curl error: "+ret.output);
 
@@ -220,11 +220,11 @@ future<string> Utils::httpPost(string url, string body, string contentType, map<
     cmd += "-d '"+body+"'";
 
     return std::async(std::launch::async, [](string cmd2){
-        string ret = ssystem(cmd2);
-        if (ret.find("curl") == string::npos)
-            return ret;
+        auto ret = ssystem(cmd2);
+        if (ret.output.find("curl") == string::npos)
+            return ret.output;
         else
-            throw std::runtime_error("Curl error: "+ret);
+            throw std::runtime_error("Curl error: "+ret.output);
 
     }, cmd);
 
