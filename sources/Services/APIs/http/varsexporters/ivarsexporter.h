7fda901e89c99f4288bbaaf583acf5df37ef39f9
Rafael Tonello <rafaeltonello55@gmail.com>
2025-09-30 22:36:36 -0300

    chore: add comments to ivarsexporter.h and jsonexporter.h

--- sources/Services/APIs/http/varsexporters/ivarsexporter.h
+++ sources/Services/APIs/http/varsexporters/ivarsexporter.h
@@ -18,16 +18,34 @@ namespace API::HTTP{
 
         virtual string escape(string source);
     public: 
+        /// @brief Add a variable to the exporter
+        /// @param name The name of the variable
+        /// @param value The value of the variable
         virtual void add(string name, DynamicVar value);
+        
+        /// @brief add multiple variables to the exporter (each tuple contains the name and the value of a variable)
+        /// @param varsToAdd a vector of tuples, where each tuple contains the name and the value of a variable
         virtual void add(vector<tuple<string, DynamicVar>> varsToAdd);
+
+        /// @brief Clear all added variables
         virtual void clear();
 
-        virtual operator string(){return string(); }
+        /// @brief serialize all added variables to a string, in the format of the exporter
+        /// @return The serialized string
+        virtual operator string(){return toString(); }
+
+        /// @brief serialize all added variables to a string, in the format of the exporter
         virtual operator const char*(){return string().c_str(); }
 
         static bool checkMimeType(string mimeType1, string mimeType2);
     public:
+
+        /// @brief Serialize all added variables to a string, in the format of the exporter
+        /// @return The serialized string
         virtual string toString() = 0;
+
+        /// @brief Get the mime type of the format of the exporter
+        /// @return The mime type as a string
         virtual string getMimeType() = 0;
     }; 
 }
--- sources/Services/APIs/http/varsexporters/jsonexporter.h
+++ sources/Services/APIs/http/varsexporters/jsonexporter.h
@@ -15,13 +15,44 @@ namespace API::HTTP {
     class JsonExporter: public IVarsExporter { 
     private:
         bool pretty;
+
+        //check if a variable has children
+        
+        
+        /// @brief Check if a variable has children
+        //for example, if we have the variables:
+        // - test.var1
+        // - test.var2
+        // - test2.var1
+        // - test2.var2.var3
+        // - test2.var2.var4
+        // - test3
+        //  we have that test and test2 has children, but test3, var1, var2, var3 and var4 has not,
+        /// @param varName The name of the variable
+        /// @return True if the variable has children, false otherwise
         bool hasChildren(string varName);
     public: 
+        /* IVarsExporter interface */
+
+        /// @brief creates a new JSON exporter.
+        /// @param pretty If true, the JSON will be pretty printed (with indentation and line breaks), otherwise it will be in a single line
         JsonExporter(bool pretty = false);
+        
+        /// @brief serialize all added variables to a JSON string
+        /// @return The serialized string
         string toString();
+
+        /// @brief Get the mime type of the format of the exporter
+        /// @return The mime type as a string
         string getMimeType();
 
+        /// @brief Get the mime type of the format of the exporter
+        /// @return The mime type as a string
         static string sGetMimeType();
+
+        /// @brief Check if the given mime type is compatible with JSON exporter
+        /// @param mimeType The mime type to check
+        /// @return True if the mime type is compatible with JSON exporter, false otherwise
         static bool checkMimeType(string mimeType);
 
     }; 
