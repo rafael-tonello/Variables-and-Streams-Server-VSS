82ad9b848a5a507dbf56f6364994af249da1f28e
Rafael Tonello <rafaeltonello55@gmail.com>
2025-10-01 22:32:02 -0300

    ci: improves dry-run detection in new version script

--- shu/pcommands/apply-or-create-new-version.sh
+++ shu/pcommands/apply-or-create-new-version.sh
@@ -14,19 +14,6 @@ source ./.shu/packages/common/misc.sh
 #10. execute git push origin main
 #11. execute git tag <version>
 #12. execute git push origin <version>
-version="$1"
-
-dryrun=false
-#check if argument --dry-run, --dryrun or -d was passed (in any position)
-for arg in "$@"; do
-    if [[ "$arg" == "--dry-run" || "$arg" == "--dryrun" || "$arg" == "-d" ]]; then
-        dryrun=true
-        #remove the argument from the list of arguments
-        set -- "${@/$arg/}"
-        break
-    fi
-done
-
 
 # Força o uso do terminal físico do usuário
 exec < /dev/tty
@@ -144,141 +131,160 @@ tryCreateVersion(){
     
 }
 
-#check if version was passed as argument
-if [ -z "$version" ]; then
-    echo "No version number supplied"
-    tryCreateVersion
-    if [ -n "$_error" ]; then
-        misc.PrintError "Error: $_error"
+main(){
+
+    dryrun=false
+    #check if argument --dry-run, --dryrun or -d was passed (in any position)
+    for arg in "$@"; do
+        if [[ "$arg" == "--dry-run" || "$arg" == "--dryrun" || "$arg" == "-d" ]]; then
+            dryrun=true
+            #remove the argument from the list of arguments
+            set -- "${@/$arg/}"
+
+            echo "remains arguments are: $@"
+            break
+        fi
+    done
+
+    version="$1"
+    #check if version was passed as argument
+    if [ -z "$version" ]; then
+        echo "No version number supplied"
+        tryCreateVersion
+        if [ -n "$_error" ]; then
+            misc.PrintError "Error: $_error"
+            exit 1
+        fi
+        version="$_r"
+    fi
+
+    #check if there are pending changes
+    if [ -n "$(git status --porcelain)" ]; then
+        misc.PrintYellow "There are pending changes, please commit or stash them before running this script"
+        git status
         exit 1
     fi
-    version="$_r"
-fi
 
-#check if there are pending changes
-if [ -n "$(git status --porcelain)" ]; then
-    misc.PrintYellow "There are pending changes, please commit or stash them before running this script"
-    git status
-    exit 1
-fi
+    #ask for confirmation
+    echo -n "Are you sure you want to apply version $version? (y/n) " > /dev/tty
+    read -r answer < /dev/tty
+    if [[ ! "$answer" =~ ^[Yy]$ ]]; then
+        echo -e "\nOperation cancelled."
+        exit 0
+    fi
 
-#ask for confirmation
-echo -n "Are you sure you want to apply version $version? (y/n) " > /dev/tty
-read -r answer < /dev/tty
-if [[ ! "$answer" =~ ^[Yy]$ ]]; then
-    echo -e "\nOperation cancelled."
+    #checkout develop
+    echo "Checking out develop branch..."
+    git checkout develop
+
+    #change version number
+    echo "Changing version number to $version in ./sources/main.cpp..."
+
+    if [ $dryrun == false ]; then
+        sed -i "s/string INFO_VERSION = \".*\"/string INFO_VERSION = \"$version\"/g" ./sources/main.cpp
+    fi
+
+    #add commits since last tag to the CHANGELOG.md file
+    lastTag=$(git describe --tags --abbrev=0 origin 2>/dev/null)
+    if [ -z "$lastTag" ]; then
+        lastTag=$(git describe --tags --abbrev=0 2>/dev/null)
+    fi
+    if [ -z "$lastTag" ]; then
+        lastTag=$(git rev-list --max-parents=0 HEAD)
+    fi
+
+    if [ $dryrun == false ]; then
+        echo "# Version $version changes" >> CHANGELOG.md
+    else
+        echo "dry run: mimicking 'echo \"# Version $version changes\" >> CHANGELOG.md'"
+    fi
+
+    for commit in $(git log --pretty=format:"%H" "$lastTag"..HEAD); do
+        local message=$(git log -1 --pretty=%B $commit)
+        echo "- $message" >> CHANGELOG.md
+    done
+
+    if [ $dryrun == false ]; then
+        echo "" >> CHANGELOG.md
+    else
+        echo "dry run: mimicking 'add a new line to CHANGELOG.md'"
+    fi
+
+    #add changes
+    echo "Adding changes to git..."
+
+    if [ $dryrun == false ]; then
+        git add ./sources/main.cpp
+        git add CHANGELOG.md
+    else
+        echo "dry run: mimicking 'git add ./sources/main.cpp' and 'git add CHANGELOG.md'"
+    fi
+
+    #commit changes
+    if [ $dryrun == false ]; then
+        git commit -m "chore: changes version number to $version and updates CHANGELOG.md"
+    else
+        echo "dry run: mimicking 'git commit -m \"chore: changes version number to $version and updates CHANGELOG.md\"'"
+    fi
+
+    #push changes
+    if [ $dryrun == false ]; then
+        git push origin develop
+    else
+        echo "dry run: mimicking 'git push origin develop'"
+    fi
+
+
+    #checkout main
+    echo "Checking out main branch..."
+    git checkout main
+
+    #merge develop
+    echo "Merging develop branch into main..."
+    if [ $dryrun == false ]; then
+        git merge develop
+    else
+        echo "dry run: mimicking 'git merge develop'"
+    fi
+
+    #push changes
+    echo "Pushing changes to origin main..."
+    if [ $dryrun == false ]; then
+        git push origin main
+    else
+        echo "dry run: mimicking 'git push origin main'"
+    fi
+
+    #create tag
+    echo "Creating tag $version..."
+    if [ $dryrun == false ]; then
+        git tag $version
+    else
+        echo "dry run: mimicking 'git tag $version'"
+    fi
+
+    #push tag
+    echo "Pushing tag $version to origin..."
+    if [ $dryrun == false ]; then
+        git push origin $version
+    else
+        echo "dry run: mimicking 'git push origin $version'"
+    fi
+
+    #checkout develop
+    git checkout develop
+
+    if [ $dryrun == false ]; then
+        echo "" > CHANGELOG.md
+        git add CHANGELOG.md
+        git commit -m "chore: creates new empty CHANGELOG.md for next version"
+        git push origin develop
+    else
+        echo "dry run: mimicking 'create new empty CHANGELOG.md, git add, git commit and git push origin develop'"
+    fi
+
+    echo "Version $version applied successfully and new CHANGELOG.md created."
     exit 0
-fi
+}
 
-#checkout develop
-echo "Checking out develop branch..."
-git checkout develop
-
-#change version number
-echo "Changing version number to $version in ./sources/main.cpp..."
-
-if [ $dryrun == false ]; then
-    sed -i "s/string INFO_VERSION = \".*\"/string INFO_VERSION = \"$version\"/g" ./sources/main.cpp
-fi
-
-#add commits since last tag to the CHANGELOG.md file
-lastTag=$(git describe --tags --abbrev=0 origin 2>/dev/null)
-if [ -z "$lastTag" ]; then
-    lastTag=$(git describe --tags --abbrev=0 2>/dev/null)
-fi
-if [ -z "$lastTag" ]; then
-    lastTag=$(git rev-list --max-parents=0 HEAD)
-fi
-
-if [ $dryrun == false ]; then
-    echo "# Version $version changes" >> CHANGELOG.md
-else
-    echo "dry run: mimicking 'echo \"# Version $version changes\" >> CHANGELOG.md'"
-fi
-
-for commit in $(git log --pretty=format:"%H" "$lastTag"..HEAD); do
-    local message=$(git log -1 --pretty=%B $commit)
-    echo "- $message" >> CHANGELOG.md
-done
-
-if [ $dryrun == false ]; then
-    echo "" >> CHANGELOG.md
-else
-    echo "dry run: mimicking 'add a new line to CHANGELOG.md'"
-fi
-
-#add changes
-echo "Adding changes to git..."
-
-if [ $dryrun == false ]; then
-    git add ./sources/main.cpp
-    git add CHANGELOG.md
-else
-    echo "dry run: mimicking 'git add ./sources/main.cpp' and 'git add CHANGELOG.md'"
-fi
-
-#commit changes
-if [ $dryrun == false ]; then
-    git commit -m "chore: changes version number to $version and updates CHANGELOG.md"
-else
-    echo "dry run: mimicking 'git commit -m \"chore: changes version number to $version and updates CHANGELOG.md\"'"
-fi
-
-#push changes
-if [ $dryrun == false ]; then
-    git push origin develop
-else
-    echo "dry run: mimicking 'git push origin develop'"
-fi
-
-
-#checkout main
-echo "Checking out main branch..."
-git checkout main
-
-#merge develop
-echo "Merging develop branch into main..."
-if [ $dryrun == false ]; then
-    git merge develop
-else
-    echo "dry run: mimicking 'git merge develop'"
-fi
-
-#push changes
-echo "Pushing changes to origin main..."
-if [ $dryrun == false ]; then
-    git push origin main
-else
-    echo "dry run: mimicking 'git push origin main'"
-fi
-
-#create tag
-echo "Creating tag $version..."
-if [ $dryrun == false ]; then
-    git tag $version
-else
-    echo "dry run: mimicking 'git tag $version'"
-fi
-
-#push tag
-echo "Pushing tag $version to origin..."
-if [ $dryrun == false ]; then
-    git push origin $version
-else
-    echo "dry run: mimicking 'git push origin $version'"
-fi
-
-#checkout develop
-git checkout develop
-
-if [ $dryrun == false ]; then
-    echo "" > CHANGELOG.md
-    git add CHANGELOG.md
-    git commit -m "chore: creates new empty CHANGELOG.md for next version"
-    git push origin develop
-else
-    echo "dry run: mimicking 'create new empty CHANGELOG.md, git add, git commit and git push origin develop'"
-fi
-
-echo "Version $version applied successfully and new CHANGELOG.md created."
-exit 0
+main "$@"
