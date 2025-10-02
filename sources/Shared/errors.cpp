86b3c5b186680e7ed5e4514a96408da461c1e9f1
Rafael Tonello <rafaeltonello55@gmail.com>
2025-10-01 22:27:30 -0300

    fix: fixes error messages texts

--- sources/Shared/errors.cpp
+++ sources/Shared/errors.cpp
@@ -4,9 +4,9 @@ namespace Errors{
     Error NoError = "";
     Error Error_VariableWithWildCardCantBeSet = "Invalid setVar parameter. A variable with '*' can't be set";
     Error Error_TheVariable_name_IsLocketAndCantBeChangedBySetVar = "The variable 'vName' is locked and can't be changed by setVar";
-    Error Error_VariablesStartedWithUnderscornAreJustForInternal = "Variabls started with underscorn (_) are just for internal flags and can't be setted by clients";
-    Error Error_WildCardCabBeUsedOnlyAtEndOfVarNameForVarGetting = "Wildcard char (*) can be used only at last of varname in the getVar function";
-    Error Error_WildcardCanotBeUsesForGetVarChilds = "Wildcard char (*) cannot be used for get childs of a var";
+    Error Error_VariablesStartedWithUnderscornAreJustForInternal = "Variabls started with underscorn (_) are just for internal flags and can't be set by clients";
+    Error Error_WildCardCabBeUsedOnlyAtEndOfVarNameForVarGetting = "Wildcard char (*) can be used only at end of varnames when getting vars";
+    Error Error_WildcardCanotBeUsesForGetVarChilds = "Wildcard char (*) cannot be used for get variable childs";
     Error Error_TheVariableNameCannotBeEmpty = "The variable name cannot be empty";
     Error Error_TimeoutReached = "Timeout reached";
 }
